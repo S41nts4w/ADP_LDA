c5d24ec495223151f4df7f41e959c7e2a53cca5c
S41nts4w <danielvondrathen@hotmail.de>
2017-04-14 15:26:46 +0200

    Test
    dies das

--- src/IterativerPascal.java
+++ src/IterativerPascal.java
@@ -1,34 +1,29 @@
 
-public class IterativerPascal implements Pascal
-{
-	
-	private long _zaehler;	
+public class IterativerPascal implements Pascal {
 
-	public long[] berechnePascalschesDreieck(int zeile)
-	{
-		long[][] pascal = new long[zeile][zeile+2];		
+	private long _zaehler;
+
+	public long[] berechnePascalschesDreieck(int zeile) {
+		long[][] pascal = new long[zeile][zeile + 2];
 		long result;
-		
+
 		pascal[0][1] = 1;
-		
-		for(int reihe = 1; reihe < zeile; reihe++)
-		{
+
+		for (int reihe = 1; reihe < zeile; reihe++) {
 			_zaehler++;
-			for(int spalte = 1; spalte < zeile+2; spalte++)
-			{
+			for (int spalte = 1; spalte < zeile + 2; spalte++) {
 				_zaehler++;
-				result = pascal[reihe-1][spalte] + pascal [reihe-1][spalte-1];
+				result = pascal[reihe - 1][spalte] + pascal[reihe - 1][spalte - 1];
 				pascal[reihe][spalte] = result;
-				if(result == 0)
-				{
+				if (result == 0) {
 					break;
 				}
 			}
 		}
-		
-		return pascal[zeile-1];
+
+		return pascal[zeile - 1];
 	}
-	
+
 	public long getZaehler() {
 		return _zaehler;
 	}
