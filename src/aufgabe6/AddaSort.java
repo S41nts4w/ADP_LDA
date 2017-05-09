db9db7c1fcd3d80a17f12859e9f886f6ac2a9b05
S41nts4w <danielvondrathen@hotmail.de>
2017-05-09 21:01:26 +0200

    AddaSort Kommentiert

--- src/aufgabe6/AddaSort.java
+++ src/aufgabe6/AddaSort.java
@@ -8,109 +8,78 @@ public class AddaSort {
 	long zeit1;
 	long zeit2;
 
-	public int[] sortiere(int[] liste) {
-		zeit1 = System.nanoTime();
-		int n = liste.length;
-		int[] swap = new int[100 * n];
-		int[] output = new int[n];
-		int j = 0;
-		for (int i = 0; i < n; i++) {
-			swap[liste[i] - 700 * n] += 1;
-			aufwandszaehler++;
-		}
-		int addierer = 0;
-		for (int i = 0; i < swap.length; i++) {
-			aufwandszaehler++;
-			swap[i] += addierer;
-			addierer = swap[i];
-		}
+	// Der Counting-Sort, der das Array nach einer bestimmten Stelle Soritert,
+	// aufgerufen vom RadixSort
+	static int counter = 0;
 
-		for (int i = 0; i < n; i++) {
-			aufwandszaehler++;
-			output[swap[liste[i] - (700 * n)] - 1] = liste[i];
-			swap[liste[i] - 700 * n]--;
-		}
-
-		// for (int i = 0; i < swap.length; i++) {
-		// while (swap[i] > 0) {
-		// aufwandszaehler++;
-		// output[j] = i + 700 * n;
-		// j++;
-		// swap[i]--;
-		// if (j == n) {
-		// zeit2 = System.nanoTime();
-		// return output;
-		// }
-		// }
-		// }
-		zeit2 = System.nanoTime();
-		return output;
-	}
-
-	private void countSort(int arr[], int n, int exp) {
+	static void countSort(int arr[], int n, int exp) {
 		int output[] = new int[n]; // output array
 		int i;
 		int count[] = new int[10];
-//		Arrays.fill(count, 0);
+		Arrays.fill(count, 0);
 
-		// Store count of occurrences in count[]
+		// Ursprungswert / die gesuchte Stelle Modulo 10 (Wegschneiden der
+		// stellen davor und dahinter) -> gesuchter Index
 		for (i = 0; i < n; i++) {
-			aufwandszaehler2++;
 			count[(arr[i] / exp) % 10]++;
+			counter++;
 		}
 
-		// Change count[i] so that count[i] now contains
-		// actual position of this digit in output[]
+		// Hochzählen der count Stellen um die echte Position herzustellen fuer
+		// das Output Array
 		for (i = 1; i < 10; i++) {
-			aufwandszaehler2++;
 			count[i] += count[i - 1];
+			counter++;
 		}
 
-		// Build the output array
+		// Fuellen des zwischen-Arrays am Index der erzeugt wird, durch den
+		// Ursprungswert / die gesuchte Stelle Modulo 10 (Wegschneiden der
+		// stellen davor und dahinter)
 		for (i = n - 1; i >= 0; i--) {
-			aufwandszaehler2++;
 			output[count[(arr[i] / exp) % 10] - 1] = arr[i];
 			count[(arr[i] / exp) % 10]--;
+			counter++;
 		}
 
-		// Copy the output array to arr[], so that arr[] now
-		// contains sorted numbers according to current digit
+		// Uebertragen des erstellten Arrays auf das Ursprungs Array
 		for (i = 0; i < n; i++) {
-			aufwandszaehler2++;
 			arr[i] = output[i];
+			// counter++;
 		}
 	}
 
-	// The main function to that sorts arr[] of size n using
-	// Radix Sort
-	public void radixsort(int arr[]) {
-		zeit1 = System.nanoTime();
-		// Find the maximum number to know number of digits
-		int n = arr.length;
-		int m = 10 * n;
+	// Radix Sort Algortihmus, fuehrt den Counting-Sort intern auf bestimmte
+	// Stellen aus
+	static void radixsort(int arr[], int n) {
+		// Beschränkung auf die gesuchte Stelle im Counting Sort
+		int m = n * 10;
 
-		// Do counting sort for every digit. Note that instead
-		// of passing digit number, exp is passed. exp is 10^i
-		// where i is current digit number
-		for (int exp = 100; m / exp > 0; exp *= 10) {
-			aufwandszaehler2++;
+		// Uebergibt die Aktuell durchsuchte Stelle (exp) fuer den Counting Sort
+		for (int exp = 1; m / exp > 0; exp *= 10) {
 			countSort(arr, n, exp);
+			counter++;
 		}
+	}
+
+	// Ausdrucken von Arrays
+	static void print(int arr[], int n) {
+		for (int i = 0; i < n; i++)
+			System.out.print(arr[i] + " ");
+	}
+
+	// einfache einordnung am Index Alternativer unsicherer Algortihmus
+	public void sort(int n, int arr[]) {
+		zeit1 = System.nanoTime();
+		int[] output = new int[n];
+		int mod = 7 * n;
+		for (int i = 0; i < arr.length; i++) {
+			int index = (arr[i] / 100) % mod;
+			output[index] = arr[i];
+			aufwandszaehler2++;
+		}
+		arr = output;
 		zeit2 = System.nanoTime();
 	}
-	
-	public void sort(int n,int arr[]){
-		zeit1 = System.nanoTime();
-        int[] output = new int[n];
-        int mod = 7*n;
-        for(int i = 0; i < arr.length;i++){
-            int index =(arr[i]/100)%mod;
-            output[index] = arr[i];
-            aufwandszaehler2++;
-        }
-        arr=output;
-        zeit2 = System.nanoTime();
-    }
 
 	public long getZeitAufwand() {
 		return zeit2 - zeit1;
